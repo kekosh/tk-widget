16675b0f545866e5f993f508a692f5c8cc438657
la0489@snet.isid.co.jp <la0489@snet.isid.co.jp>
2018-11-14 18:03:11 +0900

    add json module

--- count-up.py
+++ count-up.py
@@ -1,27 +1,66 @@
 import tkinter as tk
 from tkinter import messagebox
 import os
+import json
+import collections as cl
 
+#----------------------------------------------------
 """ create text file for save input data
     if target file is nothing, make new text file.
     if same name file is exists, add new line """
 def set_regist_data_file():
-    path = os.getcwd() + "/regdata.txt"
+    #check os type（windows:nt linux:posix）
+    if os.name == "nt":
+        path = os.getcwd() + "\\regdata.txt"
+    else:
+        path = os.getcwd() + "/regdata.txt"   
+    
+    return path
     
     #[test code]
-    with open(path,mode="r") as f:
-        if len(f.read()) < 1:
-            data = "Nothing"
-        else:
-            data = f.read()
+    # with open(path,mode="r") as f:
+        # if len(f.read()) < 1:
+            # data = "Nothing"
+        # else:
+            # data = f.read()
     
-        print(data)
+        # print(data)
 
+""" load json-file data """
+def load_jsonfile(path):
+    try:
+        with open(path, "r") as f:
+            json_data = json.load(f)
+            print(json_data["default"])
+            
+            """ json.dumps will format the output data to json style """
+            # dump = json.dumps(json_data, indent=4)
+            # print(dump)
+    except:
+        print("Error")
+
+def save_jsonfile(path):
+    o_odict = cl.OrderedDict()
+    i_odict = cl.OrderedDict()
+
+    count = lb.cget("text")
+    entry = ent.cget("text")
+    
+    i_odict["count"] = count
+    i_odict["entry"] = entry
+    o_odict["default"] = i_odict
+    
+    #print(json.dumps(o_odict, indent=4))
+    
+    with open(path, "w+") as f:
+        json.dump(o_odict, f, indent=4)
+    
+#----------------------------------------------------
 
 root = tk.Tk()
 root.title("count-up")
 #root.geometry("300x200")
-set_regist_data_file()
+
 # <- Frame1 ->
 frame1 = tk.Frame(root, width="3c", height="5c")
 frame1.propagate(False)
@@ -65,5 +104,9 @@ btn2.pack()
 # scrtxt.pack()
 
 frame2.grid(row=0, column=1)
+
+path = set_regist_data_file()
+load_jsonfile(path)
+save_jsonfile(path)
 
 root.mainloop()
