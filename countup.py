602b4aabe6e16c942378560a8b2079098f44868c
kekosh <kenta0540@gmail.com>
2018-11-19 10:51:49 +0900

    update func  'create-widget'

--- countup.py
+++ countup.py
@@ -9,13 +9,20 @@ import collections as cl
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.pack()
-        self.create_widgets()
+        #self.pack()
+        self.create_widgets(master)
 
-    def create_widgets(self):
-        #sample
-        self.bk = tk.Button(text="Button")
-        self.bk.pack()
+    def create_widgets(self, master):
+        #sample-start
+        # self.bk = tk.Button(text="Button")
+        # self.bk.pack()
+        #sample-end
+
+        self.frame = tk.Frame(master, width="150", height="100", relief=tk.SOLID, borderwidth="1")
+        self.frame.propagate(False)
+        self.frame.grid(row=0)
+
+
 
     def load_json(self):
         data = load.json()
@@ -25,7 +32,7 @@ class Application(tk.Frame):
 
 #---------------------------------
 root = tk.Tk()
-#root.title("count up")
+root.title("count up")
 root.geometry("300x200")
 app = Application(master=root)
 app.mainloop()
