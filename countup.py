a9a5e20152a7671ac276df47ecb52e090af0c5ab
kekosh <kenta0540@gmail.com>
2018-11-19 12:55:11 -0200

    model of json data file is fixed and new function load jsonfile

--- countup.py
+++ countup.py
@@ -9,18 +9,30 @@ import collections as cl
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        #self.pack()
-        self.create_widgets(master)
+        data_file = self.get_filepath(os.name)
 
-    def create_widgets(self, master):
+        with open(data_file,"r") as f:
+            json_data = json.load(f)
+            
+        self.create_widgets(master,json_data)
+
+
+    def get_filepath(self, os_name):
+        dirkey = "\\" if os_name == "nt" else "/"
+        return "{0}{1}data.json".format(os.getcwd(),dirkey)
+
+
+    def create_widgets(self, master, json_data):
         #sample-start
         # self.bk = tk.Button(text="Button")
         # self.bk.pack()
         #sample-end
 
-        self.frame = tk.Frame(master, width="150", height="100", relief=tk.SOLID, borderwidth="1")
+        self.frame = tk.Frame(master, width="800", height="80", relief=tk.SOLID, borderwidth="1")
         self.frame.propagate(False)
         self.frame.grid(row=0)
+
+
 
 
 
@@ -33,6 +45,7 @@ class Application(tk.Frame):
 #---------------------------------
 root = tk.Tk()
 root.title("count up")
-root.geometry("300x200")
+root.geometry("800x400")
+root.resizable(0,1)
 app = Application(master=root)
 app.mainloop()
