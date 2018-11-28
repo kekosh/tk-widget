c0769a833cf16e79d4832669ada9af4d57c2e172
kekosh <kenta0540@gmail.com>
2018-11-28 12:12:19 -0200

    updata. add title header and up/down button template

--- countup.py
+++ countup.py
@@ -28,17 +28,28 @@ class Application(tk.Frame):
         odict = cl.OrderedDict()
         odict = json_data["data"]
         for value in odict.values():
-            self.frame = tk.Frame(master, width="780",relief=tk.SOLID, borderwidth="1")
+            self.frame = tk.Frame(master, width="500",relief=tk.SOLID, borderwidth="1")
             self.frame.propagate(False)
             self.frame.pack(padx=5, pady=10, fill=tk.X)
 
+            self.header_lbl_1 = tk.Label(self.frame, text="count")
+            self.header_lbl_1.grid(row=0, column=0)
             self.label = tk.Label(self.frame, text=str(value["count"]))
-            self.label.grid(row=0, column=0, padx=20)
-            self.entry = tk.Entry(self.frame, width=60, borderwidth="1", relief=tk.SOLID)
-            self.entry.grid(row=0, column=1, padx=10)
-            self.btn = tk.Button(self.frame, width=20, text="Up")
-            self.btn.grid(row=0, column=2)
+            self.label.grid(row=1, column=0, padx=20)
 
+
+            self.header_lbl_2 = tk.Label(self.frame, text="entry")
+            self.header_lbl_2.grid(row=0, column=1)
+            self.entry = tk.Entry(self.frame, width=40, borderwidth="1", relief=tk.SOLID)
+            self.entry.grid(row=1, column=1, padx=10)
+
+
+            self.header_lbl_3 = tk.Label(self.frame, text="Up/Down")
+            self.header_lbl_3.grid(row=0, column=2, columnspan=2)
+            self.up_btn = tk.Button(self.frame, width=5, text="↑")
+            self.up_btn.grid(row=1, column=2)
+            self.down_btn = tk.Button(self.frame, width=5, text="↓")
+            self.down_btn.grid(row=1, column=3)
 
 #---------------------------------
 
@@ -46,7 +57,7 @@ class Application(tk.Frame):
 #---------------------------------
 root = tk.Tk()
 root.title("count up")
-root.geometry("800x400")
+root.geometry("550x400")
 root.resizable(1,1) #0:unresizable 1: resizable
 app = Application(master=root)
 app.mainloop()
